5f96a02d9340ac6149de515b64e8ded5e507130e
unknown <fajrinsuhar97@gmail.com>
2025-03-28 15:38:29 +0800

    feat: Menambahkan UseState

--- src/components/Fragments/CardProducts.jsx
+++ src/components/Fragments/CardProducts.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button";
 const CardProducts = (props) => {
     const {children} = props;
     return(
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow flex flex-col justify-between ">
+    <div className="w-full max-w-xs bg-gray-800 border border-gray-700 rounded-lg shadow mx-4 my-4 flex flex-col justify-between ">
         {children}
      </div>
   
@@ -35,11 +35,11 @@ const Body = (props) =>{
 
 
   const Footer = (props) => {
-    const {price, children} = props;
+    const {price, handleAddToCart, id} = props;
     return(
     <div className="flex items-center justify-between px-5 pb-5">
-        <span className=" text-xl font-bold text-white">{price}</span>
-        <Button classname="bg-blue-600"> Add To Chart</Button>
+        <span className=" text-xl font-bold text-white">Rp {price.toLocaleString("id-ID",{styles: "currency", currency: "IDR"})}</span>
+        <Button classname="bg-blue-600" onClick={ () => handleAddToCart(id) }> Add To Chart</Button>
     </div>
     );
   };
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,5 +1,5 @@
 import Button from "../components/Elements/Button";
-
+import { useState } from "react";
 import CardProducts from "../components/Fragments/cardProducts";
 import Counter from "../components/Fragments/Counter";
 
@@ -7,35 +7,75 @@ import Counter from "../components/Fragments/Counter";
 const products = [
   {
     id: 1,
-    name: "Sepatu Baru",
-    price: "Rp 1.000.000",
-    image: "/img/sepatu-1.jpg",
+    name: "Burger Baru",
+    price: 100000,
+    image: "/img/burger.png",
     description: `Sepatu ini terbuat dari kulit emas asli korea yang sangat amat bagus`
   },
   {
     id: 2,
-    name: "Sepatu lama",
-    price: "Rp 500.000",
-    image: "/img/sepatu-1.jpg",
-    description: `Sepatu ini terbuat dari kulit emas asli korea yang sangat amat bagus Sepatu ini terbuat dari kulit emas asli korea yang sangat amat bagus`
+    name: "burger lama",
+    price: 500000,
+    image: "/img/burger.png",
+    description: `burger ini terbuat dari kulit emas asli korea yang sangat amat bagus burger ini terbuat dari kulit emas asli korea yang sangat amat bagus`
   },
   {
     id: 3,
-    name: "Sepatu lama",
-    price: "Rp 500.000",
-    image: "/img/sepatu-1.jpg",
-    description: `Sepatu ini terbuat dari kulit emas asli korea yang sangat amat bagus Sepatu ini terbuat dari kulit emas asli korea yang sangat amat bagus`
+    name: "burger lama",
+    price: 500000,
+    image: "/img/burger.png",
+    description: `burger ini terbuat dari kulit emas asli korea yang sangat amat bagus Sepatu ini terbuat dari kulit emas asli korea yang sangat amat bagus`
+  },
+  {
+    id: 4,
+    name: "burger abcd",
+    price: 510000,
+    image: "/img/burger.png",
+    description: `burger ini terbuat dari kulit emas asli korea yang sangat amat bagus Sepatu ini terbuat dari kulit emas asli korea yang sangat amat bagus`
   }
 ]
 
 
-const email = localStorage.getItem('email')
+
+const email  = localStorage.getItem('email')
+
 const HandleLogout = () => {
   window.location.href ="/login"
   
 }
 
+
+
 const ProductsPage = () => {
+
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      qty: 1,
+
+
+    }
+  ]);
+
+  const HandleLogout = () => {
+    window.location.href ="/login"
+    
+  }
+
+
+  const handleAddToCart = (id) => {
+   if(cart.find(item => item.id === id)){
+    setCart(
+      cart.map(
+        item => item.id === id ? {...item,  qty: item.qty + 1 } : item) 
+      )
+    } else{
+        setCart([...cart, {id, qty: 1}])
+       };
+   
+  };
+
+
   return(
     <>
     <div className="flex justify-end h-10 bg-blue-600  text-white px-16 ">
@@ -43,16 +83,48 @@ const ProductsPage = () => {
     <Button className="my-auto " onClick={HandleLogout}>Logout</Button>
     </div>
 
-    <div className="flex justify-center py-5 space-x-7 ">
-    {products.map((product) => 
+    <div className="flex justify-center py-5 space-x-7">
+    <div className="w-3/4 flex flex-wrap">
+                {products.map((product) => 
 
-        <CardProducts key={product.id}>
-        <CardProducts.Header Image={product.image}></CardProducts.Header>
-          <CardProducts.Body name={product.name} children={product.description}></CardProducts.Body>
-          <CardProducts.Footer price={product.price}></CardProducts.Footer>
-        </CardProducts>
-    )}
+            <CardProducts key={product.id}>
+            <CardProducts.Header Image={product.image}></CardProducts.Header>
+              <CardProducts.Body name={product.name} children={product.description}></CardProducts.Body>
+              <CardProducts.Footer price={product.price} id={product.id} handleAddToCart={handleAddToCart}/>
+            </CardProducts>
+            )}
     </div>
+      
+      <div className="w-1/4 ">
+          <h1 className="text-3xl text-blue-600 font-bold">Cart</h1>
+
+        <table className=" text-center border-separate border-spacing-x-2 ">
+          <thead>
+              <tr>
+                <th>product</th>
+                <th>price</th>
+                <th>Quantity</th>
+                <th>total</th>
+              </tr>
+          </thead>
+          <tbody>
+            {cart.map((item) => {
+          
+          const product = products.find((product) => product.id == item.id);
+          return (
+          <tr key={item.id}>
+              <td>{product.name}</td>
+              <td>Rp {(product.price.toLocaleString("ID-id" ,{styles: "currency", currency:"IDR"}))}</td>
+              <td>{item.qty}</td>
+              <td>Rp {(item.qty * product.price).toLocaleString("ID-id", {styles: "currency", currency:"IDR"})}</td>
+          </tr>
+          )
+          })} 
+          </tbody>
+        </table>
+      </div>
+    </div>
+
 
     <div className=" flex justify-center">
     <Counter></Counter>
@@ -60,7 +132,6 @@ const ProductsPage = () => {
     </>
   ); 
 };
-
 
 
 export default ProductsPage;
